7c67e22c7c3a74d56570464152eaca7aba4d039a
Paul McManus <paulmcmanus2021@hotmail.com>
2019-11-23 12:28:06 +0000

    CRUD for ticket

--- models/ticket.rb
+++ models/ticket.rb
@@ -29,6 +29,12 @@ class Ticket
     SqlRunner.run(sql,values)
   end
 
+  def update()
+    sql = "UPDATE tickets SET (customer_id, film_id) = ($1, $2) WHERE id = $3;"
+    values = [@customer_id, @film_id,@id]
+    SqlRunner.run(sql, values)
+  end
+
 
 
 
