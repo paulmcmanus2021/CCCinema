97f5bf8d447b2df4519aab483e8cef3e3b6f8116
Paul McManus <paulmcmanus2021@hotmail.com>
2019-11-23 11:43:22 +0000

    Adds films and saving films

--- console.rb
+++ console.rb
@@ -5,24 +5,43 @@ require_relative('models/film')
 require('pry-byebug')
 
 Customer.delete_all()
+# Film.delete_all()
 
+#customers
   customer1 = Customer.new({
     'name' => 'Joel Robinson',
     'funds' => 20
     })
     customer1.save()
-
   customer2 = Customer.new({
     'name' => 'Crow T. Robot',
     'funds' => 15
     })
     customer2.save()
-
   customer3 = Customer.new({
     'name' => 'Tom Servo',
     'funds' => 10
     })
     customer3.save()
+
+#films
+  film1 = Film.new({
+    'title' => 'This Island Earth',
+    'price' => 4
+    })
+    film1.save()
+
+  film2 = Film.new({
+    'title' => 'The Crawling Eye',
+    'price' => 5
+    })
+    film2.save()
+
+  film3 = Film.new({
+    'title' => 'Mitchell',
+    'price' => 6
+    })
+    film3.save()
 
 binding.pry
 nil
---
+++ models/film.rb
@@ -0,0 +1,34 @@
+require_relative('../db/sql_runner')
+
+class Film
+  attr_reader :id
+  attr_accessor :title, :price
+
+  def initialize(details)
+    @id = details['id'].to_i if details['id']
+    @title = details['title']
+    @price = details['price'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO films (title, price) VALUES ($1, $2) RETURNING id;"
+    values = [@title, @price]
+    film = SqlRunner.run(sql, values)[0]
+    @id = film['id'].to_i
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+end
